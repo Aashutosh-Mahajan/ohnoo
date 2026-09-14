5b048ec0b26e0b1b4103d440792e18a5b6a0256a
Aashutosh-Mahajan <aashutoshmahajan.2007@gmail.com>
2026-09-14 19:50:42 +0530

    Add site footer

---
+++ landing/src/components/SiteFooter.jsx
@@ -0,0 +1,10 @@
+export default function SiteFooter() {
+  return (
+    <footer className="site-footer">
+      MIT licensed. Built for the post-crash moment.{" "}
+      <a href="https://github.com/Aashutosh-Mahajan/ohnoo" target="_blank" rel="noreferrer">
+        github.com/Aashutosh-Mahajan/ohnoo
+      </a>
+    </footer>
+  );
+}
